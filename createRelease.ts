8aa12629d874c2502b6060d18a8b6f54ea55f51b
ynhhoJ <22500212+ynhhoJ@users.noreply.github.com>
2025-04-13 15:31:32 +0300

    feat: Update `createRelease` script to actualize files in children folders

--- createRelease.ts
+++ createRelease.ts
@@ -10,7 +10,24 @@ import iconv from "iconv-lite";
 		const xml = await file.text();
 		const encoded = iconv.encode(xml, "win1251");
 
-		await Bun.write(`./release/${fileName}`, encoded);
+		await Bun.write(
+			`./release/287- G.A.M.M.A. Massive Text Overhaul Project - SageDaHerb and Dr.Pr1nkos/gamedata/configs/text/rus/${fileName}`,
+			encoded,
+		);
+
+		if (fileName === "ui_st_jamming.xml") {
+			await Bun.write(
+				`./release/G.A.M.M.A. Unjam Reload on the same key/gamedata/configs/text/rus/${fileName}`,
+				encoded,
+			);
+		}
+
+		if (fileName === "st_indirect_parts_favoriter.xml") {
+			await Bun.write(
+				`./release/G_FLAT's Indirect Parts Favoriter/gamedata/configs/text/rus/${fileName}`,
+				encoded,
+			);
+		}
 
 		console.log(fileName);
 	}
