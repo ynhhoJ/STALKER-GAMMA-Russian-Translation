5f2a4bab1ba56a0074f0f9a82266dbd01561ee2b
ynhhoJ <22500212+ynhhoJ@users.noreply.github.com>
2025-05-26 19:35:26 +0300

    chore: Build release script

--- createRelease.ts
+++ createRelease.ts
@@ -11,37 +11,10 @@ import iconv from "iconv-lite";
 		const encoded = iconv.encode(xml, "win1251");
 
 		await Bun.write(
-			`./release/287- G.A.M.M.A. Massive Text Overhaul Project - SageDaHerb and Dr.Pr1nkos/gamedata/configs/text/rus/${fileName}`,
+			`./release/G.A.M.M.A Actualized Russian Translations/gamedata/configs/text/rus/${fileName}`,
 			encoded,
 		);
 
-		if (fileName === "ui_st_jamming.xml") {
-			await Bun.write(
-				`./release/G.A.M.M.A. Unjam Reload on the same key/gamedata/configs/text/rus/${fileName}`,
-				encoded,
-			);
-		}
-
-		if (fileName === "st_indirect_parts_favoriter.xml") {
-			await Bun.write(
-				`./release/G_FLAT's Indirect Parts Favoriter/gamedata/configs/text/rus/${fileName}`,
-				encoded,
-			);
-		}
-
 		console.log(fileName);
 	}
-
-	// const englishTranslationFiles = await readdir("./eng-utf8/");
-	//
-	// for (const fileName of englishTranslationFiles) {
-	// 	const file = Bun.file(`./eng-utf8/${fileName}`);
-	//
-	// 	const xml = await file.text();
-	// 	const encoded = iconv.encode(xml, "win1251");
-	//
-	// 	await Bun.write(`./eng-release/${fileName}`, encoded);
-	//
-	// 	console.log(fileName);
-	// }
 })();
